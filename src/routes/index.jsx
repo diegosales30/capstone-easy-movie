ac25c5f6af61e96484ba65c1bd112237372f1526
Rafael Correa <rafaelclramos1813@gmail.com>
2022-05-14 14:26:32 -0300

    header

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "../pages/Header";
+import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 
