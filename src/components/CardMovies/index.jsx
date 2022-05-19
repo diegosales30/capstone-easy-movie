ceec346b9b995e70fe22432bf8c07642409256fb
diegosales30 <diegofelipesales23@gmail.com>
2022-05-19 16:58:39 -0400

    fix: resolvendo bug que quebrava o app e nao listava o carrosel

--- src/components/CardMovies/index.jsx
+++ src/components/CardMovies/index.jsx
@@ -22,7 +22,7 @@ import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { listMoviesThunk } from "../../store/modules/movies/thunk";
 import { toast } from "react-toastify";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Rating = ({ rating, numReviews }) => {
   return (
