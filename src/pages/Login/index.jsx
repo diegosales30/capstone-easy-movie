1a40ec6a156f343440078196b2a267d99ce9dffd
Rafael Correa <rafaelclramos1813@gmail.com>
2022-05-19 11:51:50 -0300

    edit: CSS mobile na page login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -34,9 +34,12 @@ const Login = () => {
         h="8vh"
         align={"center"}
         justify={"space-between"}
+        sx={{"@media(max-width: 768px)":{
+          justifyContent:'center'
+  }}}
       >
-        <Heading fontSize={"20px"} justify={"center"} color={"#E50914"}>
-          <Image src={logo} w={"300px"} />
+        <Heading fontSize={"20px"} justify={"center"} color={"#E50914"} >
+          <Image src={logo} w={"300px"} h={"8.5vh"} />
         </Heading>
         {/* <HStack>
           <Button onClick={toggleColorMode}>
@@ -44,8 +47,8 @@ const Login = () => {
           </Button>
         </HStack> */}
       </HStack>
-      <Center h="90vh" minW="25%" w='500px'>
-        <VStack spacing={5} p={10} bg={"rgba(0,0,0,.75)"} w="100%" h="75%">
+      <Center h="90vh" maxW="500px" minW="320px" w="80vw">
+        <VStack spacing={5} p={0} bg={"rgba(0,0,0,.75)"} w="100%" h="60vh" padding="30px 0px">
           <Heading fontSize={"4xl"} justify={"center"} color={"#E50914"}>
             Entrar
           </Heading>
