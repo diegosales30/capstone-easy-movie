b2c46859de9e6dda7dc44fcc8395474f270df3a2
diegosales30 <diegofelipesales23@gmail.com>
2022-05-18 22:18:17 -0400

    refatorando footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -8,18 +8,15 @@ import {
   IconButton,
   Stack,
   HStack,
-  useColorModeValue
-} from '@chakra-ui/react'
-import { FaCopyright, FaTwitter, FaFacebook} from 'react-icons/fa';
-import { FcGoogle } from 'react-icons/fc'
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { FaCopyright, FaTwitter, FaFacebook } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
 
 const Footer = () => {
-  return(
-    <VStack
-      w={'100%'}
-      spacing={0}
-    >
-      <Center
+  return (
+    <VStack w={"100%"} spacing={0}>
+      {/* <Center
         bg={useColorModeValue("gray.100", "gray.900")}
         borderTopWidth={1}
         borderStyle={'solid'}
@@ -35,33 +32,41 @@ const Footer = () => {
             <Button variant={'link'}>Contact</Button>
           </HStack>
         </VStack>
-      </Center>
+      </Center> */}
       <HStack
-        w={'100%'}
-        h={'6vh'}
-        align={'center'}
-        justify='space-between'
+        w={"100%"}
+        h={"6vh"}
+        align={"center"}
+        justify="space-between"
         pl={4}
         pr={4}
         borderTop={1}
-        borderStyle={'solid'}
-        borderColor={useColorModeValue('gray.200', 'gray.700')}
+        borderStyle={"solid"}
+        borderColor={useColorModeValue("gray.200", "gray.700")}
         bg={useColorModeValue("gray.200", "gray.900")}
       >
-        <Flex
-          align={'center'}
-        >
-          <FaCopyright/>
-          <Text ml='0.5rem'>2022 IFood Kenzie - Team 4. All rights reserved.</Text>
+        <Flex align={"center"}>
+          <FaCopyright />
+          <Text ml="0.5rem">
+            2022 IFood Kenzie - Team 4. All rights reserved.
+          </Text>
         </Flex>
-        <Stack direction={'row'} spacing={4}>
-          <IconButton borderRadius={'20px'} colorScheme={'twitter'} icon={<FaTwitter/>}/>
-          <IconButton borderRadius={'20px'} colorScheme={'facebook'} icon={<FaFacebook/>}/>
-          <IconButton borderRadius={'20px'} icon={<FcGoogle/>}/>
+        <Stack direction={"row"} spacing={4}>
+          <IconButton
+            borderRadius={"20px"}
+            colorScheme={"twitter"}
+            icon={<FaTwitter />}
+          />
+          <IconButton
+            borderRadius={"20px"}
+            colorScheme={"facebook"}
+            icon={<FaFacebook />}
+          />
+          <IconButton borderRadius={"20px"} icon={<FcGoogle />} />
         </Stack>
       </HStack>
     </VStack>
-  )
-}
+  );
+};
 
 export default Footer;
